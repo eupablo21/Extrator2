1679b634fac25692ef5d8a10244f3105aaed9e64
eupablo21 <pablo.mrowskovski@escola.pr.gov.br>
2025-08-07 11:46:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavra = document.querySelector ('#botao-palavrachave');
+botaoMostraPalavra.addEventListener('click',mostraPalavraChave);
+function mostraPalavraChave(){
+alert ('foi clicado');
+}
